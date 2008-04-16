779210bfe9a56db5e19491d6041fad4683c34d89
tfelder <info@bitctrl.de>
2008-04-16 08:55:12 +0000

    DefaultUncaughtExceptionHandler verschoben (jetzt sollte wieder alles abstürzen, wenn ein Thread abstürzt)
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.fehlertls@8280 40c0be50-5d29-0410-901d-fb59edb5cafc

--- src/de/bsvrz/dua/fehlertls/fehlertls/DeFaApplikation.java
+++ src/de/bsvrz/dua/fehlertls/fehlertls/DeFaApplikation.java
@@ -157,6 +157,18 @@ public class DeFaApplikation implements StandardApplication {
 	 * {@inheritDoc}
 	 */
 	public void parseArguments(ArgumentList argumente) throws Exception {
+		
+		Thread.setDefaultUncaughtExceptionHandler(new Thread.
+				UncaughtExceptionHandler() {
+			public void uncaughtException(@SuppressWarnings("unused")
+					Thread t, Throwable e) {
+				Debug.getLogger().error("Applikation wird wegen" +  //$NON-NLS-1$
+						" unerwartetem Fehler beendet", e);  //$NON-NLS-1$
+				e.printStackTrace();
+				Runtime.getRuntime().exit(-1);
+			}
+		});
+		
 		Debug.init("DE Fehleranalyse fehlende Messdaten", argumente); //$NON-NLS-1$
 
 		this.geraetePids = argumente
@@ -172,16 +184,6 @@ public class DeFaApplikation implements StandardApplication {
 	 *            Argumente der Kommandozeile
 	 */
 	public static void main(String[] argumente) {
-		Thread
-				.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-					public void uncaughtException(@SuppressWarnings("unused")
-					Thread t, Throwable e) {
-						LOGGER.error("Applikation wird wegen" + //$NON-NLS-1$
-								" unerwartetem Fehler beendet", e); //$NON-NLS-1$
-						e.printStackTrace();
-						Runtime.getRuntime().exit(0);
-					}
-				});
 		StandardApplicationRunner.run(new DeFaApplikation(), argumente);
 	}
 
